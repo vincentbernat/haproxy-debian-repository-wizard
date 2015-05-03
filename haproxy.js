1a1e79e7e1f9262b6016a120a09ca96df9a6c25a
Vincent Bernat <vincent@bernat.im>
2015-05-03 22:16:03 +0200

    versions: add 1.5 for Debian Squeeze/Wheezy
    Wait a bit for Jessie, we will use backports as expected.

--- haproxy.js
+++ haproxy.js
@@ -19,8 +19,8 @@ angular.module('haproxy', [])
     ];
     // + means latest version, - means a stable version
     var matrix = {
-      squeeze: { '1.4': 'hdn+', '1.5': 'backports-sloppy-' },
-      wheezy: { '1.4': 'hdn+', '1.5': 'backports-' },
+      squeeze: { '1.4': 'hdn+', '1.5': 'backports-sloppy-|hdn+' },
+      wheezy: { '1.4': 'hdn+', '1.5': 'backports-|hdn+' },
       jessie: { '1.4': 'hdn+', '1.5': 'official-' },
       precise: { '1.4': 'official-|ppa+', '1.5': 'ppa+' },
       trusty: { '1.4': 'official-|ppa+', '1.5': 'backports-|ppa+' }
