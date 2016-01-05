d66d5493196e9e12b049a99a6d5fdc7dd4720554
Vincent Bernat <vincent@bernat.im>
2016-01-05 19:45:59 +0100

    js: HAProxy 1.6 is now in jessie-backports

--- haproxy.js
+++ haproxy.js
@@ -25,7 +25,7 @@ angular.module('haproxy', [])
     var matrix = {
       squeeze: { '1.4': 'hdn+',           '1.5': 'backports-sloppy-|hdn+' },
       wheezy:  { '1.4': 'hdn+',           '1.5': 'backports-|hdn+',              '1.6': 'hdn+' },
-      jessie:  { '1.4': 'hdn+',           '1.5': 'official-|hdn+',               '1.6': 'hdn+' },
+      jessie:  { '1.4': 'hdn+',           '1.5': 'official-|hdn+',               '1.6': 'backports+|hdn+' },
       sid:     {                                                                 '1.6': 'official+' },
       precise: { '1.4': 'official-|ppa+', '1.5': 'ppa+',                         '1.6': 'ppa+' },
       trusty:  { '1.4': 'official-',      '1.5': 'backports-|ppa+',              '1.6': 'ppa+' },
