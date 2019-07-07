faf087debcab58a59310e5824a5925584a3c9c1b
Vincent Bernat <bernat@debian.org>
2019-07-07 10:43:53 +0200

    gpg: do not export all signatures with key
    This is the output of:
    
        gpg --armor --export-options export-minimal \
                    --export \
           'AEF2 3487 66F3 71C6 89A7 3600 95A4 2FE8 3535 25F9'

--- haproxy.js
+++ haproxy.js
@@ -32,7 +32,7 @@ angular.module('haproxy', [])
                  '1.8': 'hdn+' },
       stretch: {                           '1.6': 'hdn+', '1.7': 'official-|hdn+',
                  '1.8': 'backports+|hdn+', '1.9': 'hdn+', '2.0': 'hdn+' },
-      buster:  { '1.8': 'official-',                      '2.0': 'hdn+' },
+      buster:  { '1.8': 'official-|hdn+',                 '2.0': 'hdn+' },
       sid:     { '1.8': 'official+',       '2.0': 'experimental' },
       // Ubuntu
       trusty:  { '1.5': 'backports-|ppa+', '1.6': 'ppa+',           '1.7': 'ppa+',
