2e50f2bba0394a9d047e8c0e9d523f45ee8874ba
Vincent Bernat <vincent@bernat.ch>
2019-08-11 14:38:47 +0200

    js: in buster-backports, 2.0

--- haproxy.js
+++ haproxy.js
@@ -31,7 +31,7 @@ angular.module('haproxy', [])
                  '1.8': 'hdn+' },
       stretch: {                           '1.6': 'hdn+', '1.7': 'official-|hdn+',
                  '1.8': 'backports+|hdn+', '1.9': 'hdn+', '2.0': 'hdn+' },
-      buster:  { '1.8': 'official-|hdn+',                 '2.0': 'hdn+' },
+      buster:  { '1.8': 'official-|hdn+',                 '2.0': 'backport+|hdn+' },
       sid:     { '2.0': 'official+' },
       // Ubuntu
       trusty:  { '1.5': 'backports-|ppa+', '1.6': 'ppa+',           '1.7': 'ppa+',
